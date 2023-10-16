89412de9a40c53940034ea304b9f7881ade782ff
jlauza <lauzajayson@gmail.com>
2023-10-16 23:13:00 +0800

    update to nullable

--- database/migrations/2023_10_16_145430_create_rooms_table.php
+++ database/migrations/2023_10_16_145430_create_rooms_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             $table->string('name')->required();
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
