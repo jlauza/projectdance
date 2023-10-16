af048682f1e02d70720ca499ebead9a35d81f39f
jlauza <lauzajayson@gmail.com>
2023-10-16 22:50:09 +0800

    initial commit - define route for courses

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/courses', function () {
+    return Inertia::render('Courses');
+})->middleware(['auth', 'verified'])->name('courses');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
