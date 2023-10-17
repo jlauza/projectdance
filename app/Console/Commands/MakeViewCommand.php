2012c5498aedfda5997de4af4b55fba644dec53d
jlauza <lauzajayson@gmail.com>
2023-10-18 00:22:38 +0800

    created makeViewCommand

--- app/Console/Commands/MakeViewCommand.php
+++ app/Console/Commands/MakeViewCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use File;
 
 class MakeViewCommand extends Command
 {
@@ -11,20 +12,58 @@ class MakeViewCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'app:make-view-command';
+    // protected $signature = 'app:make-view-command';
+    protected $signature = 'make:view {view}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Create a new blade template.';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $view = $this->argument('view');
+        $path = $this->viewPath($view);
+        $this->createDir($path);
+
+        if (File::exists($path)) {
+            $this->error("File {$path} already exist!");
+
+            return;
+        }
+
+        File::put($path, $path);
+        $this->info("File {$path} created.");
     }
+
+  /**
+   * Get the view full path.
+   *
+   * @param string $view
+   *
+   * @return string
+   */
+  public function viewPath($view) {
+    $view = str_replace(".", "/", $view) . '.blade.php';
+    $path = "resources/views/{$view}";
+
+    return $path;
+  }    
+
+  /**
+   * Create a view directory if it does not exist.
+   *
+   * @param $path
+   */  
+  protected function createDir($path) {
+    $dir = dirname($path);
+    if(!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+    }
+  }
 }
