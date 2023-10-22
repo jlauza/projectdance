7bfecec09c9d35773f4c5851bca976887093df42
jlauza <lauzajayson@gmail.com>
2023-10-22 19:41:40 +0800

    fix roomlist

--- resources/js/Pages/Rooms/RoomsList.jsx
+++ resources/js/Pages/Rooms/RoomsList.jsx
@@ -7,10 +7,16 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Checkbox } from '@mui/material';
+import { Button } from '@mui/base';
 
-export default function RoomsList({ rooms }) {
-
+export default function RoomsList() {
   console.log(rooms);
+
+  const rooms = [
+    {name: 'Room A', description: 'Test 1'},
+    {name: 'Room B', description: 'Test 2'},
+    {name: 'Room C', description: 'Test 3'}
+  ]
 
   return (
     <TableContainer component={Paper}>
@@ -33,6 +39,12 @@ export default function RoomsList({ rooms }) {
             >
               <TableCell component="th" scope="row">
                 {row.name}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {row.description}
+              </TableCell>              
+              <TableCell component="th" scope="row">
+                <Button>Edit</Button>
               </TableCell>
             </TableRow>
           ))}
